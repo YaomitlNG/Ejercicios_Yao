20ff9a502ee8f8af355f717198e7cf9d70fb3f8c
YaomitlNG <169081496+YaomitlNG@users.noreply.github.com>
2024-05-08 16:18:57 -0600

    Add files via upload

---
+++ Ejercicios_Adrian/EJERCICIO1/Pseudocodigo_Resta.py
@@ -0,0 +1,13 @@
+Inicio del pseudocódigo
+    // Se definen los números que se van a restar
+    num1 = 5050
+    num2 = 379
+    num3 = 598
+    num4 = 123
+
+    // Restar los cuatro números
+    resultado = num1 - num2 - num3 - num4
+
+    // Imprimir el resultado
+    Imprimir "El resultado de la resta es:", resultado
+Fin del pseudocódigo
