8be1601ba67e194cccc2c0ac40e84967a2bf1e40
YaomitlNG <169081496+YaomitlNG@users.noreply.github.com>
2024-05-10 12:11:32 -0600

    Add files via upload

---
+++ Ejercicios_Adrian/EJERCICIO1/Codigo_RESTA_VARIABLES.py
@@ -0,0 +1,7 @@
+num1 = int(input("Ingresado_por_Usuario: "))
+num2 = int(input("Ingresado_por_Usuario: "))
+num3 = int(input("Ingresado_por_Usuario: "))
+num4 = int(input("Ingresado_por_Usuario: "))
+
+resta = num1 - num2 - num3 - num4
+print("La resta de los cuatro números es:", resta)
