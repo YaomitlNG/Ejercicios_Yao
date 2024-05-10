a91699cc97cc4ebae558147d35e792823ece8f7b
YaomitlNG <169081496+YaomitlNG@users.noreply.github.com>
2024-05-10 12:17:39 -0600

    Add files via upload

---
+++ Ejercicios_Adrian/EJERCICIO1/Codigo_MULTIPLICACION_VARIABLES.py
@@ -0,0 +1,7 @@
+num1 = int(input("Ingresado_por_Usuario: "))
+num2 = int(input("Ingresado_por_Usuario: "))
+num3 = int(input("Ingresado_por_Usuario: "))
+num4 = int(input("Ingresado_por_Usuario: "))
+
+multiplicar = num1 * num2 * num3 * num4
+print("La multiplicación de los cuatro números es:", multiplicación)
