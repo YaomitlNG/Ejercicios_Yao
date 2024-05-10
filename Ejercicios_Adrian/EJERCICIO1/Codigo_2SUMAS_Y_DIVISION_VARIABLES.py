4bbcb39f7d1d34f143308de2eeaeffd0b4a5fa12
YaomitlNG <169081496+YaomitlNG@users.noreply.github.com>
2024-05-10 13:24:45 -0600

    Add files via upload

---
+++ Ejercicios_Adrian/EJERCICIO1/Codigo_2SUMAS_Y_DIVISION_VARIABLES.py
@@ -0,0 +1,12 @@
+num1 = int(input("Ingresado_por_Usuario: "))
+num2 = int(input("Ingresado_por_Usuario: "))
+num3 = int(input("Ingresado_por_Usuario: "))
+num4 = int(input("Ingresado_por_Usuario: "))
+
+suma1 = num1 + num2 
+suma2 = num3 + num4
+division = suma1 / suma2
+
+print("La suma de los números 1 y 2 es:", suma1)
+print("La suma de los números 3 y 4 es:", suma2)
+print("La division de la suma 1 y 2 es:", division)
