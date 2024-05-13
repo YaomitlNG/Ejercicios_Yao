5883bd5fca38a702513b2dc90f986feb560a6998
YaomitlNG <yaoastharot@gmail.com>
2024-05-13 15:28:52 -0600

    Suma

---
+++ Ejercicios_Adrian/EJERCICIO1/3Numeros_con_promedio.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Ingrese el primer número entero: "))
+numero2 = int(input("Ingrese el segundo número entero: "))
+numero3 = int(input("Ingrese el tercer número entero: "))
+
+promedio = (numero1 + numero2 + numero3) / 3
+
+print("El promedio de los tres números es:", promedio)
